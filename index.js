32e8bdac295625afafa07ea7795596b6d02a77de
hesham <hishammohmed12345@gmail.com>
2024-09-07 13:58:17 +0300

    feat : add test2

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get("/test", (req, res) => {
   res.send("test");
 });
 
+app.post("/test2", (req, res) => {
+  res.send("test3");
+});
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
